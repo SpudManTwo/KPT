6ddaee7a4dd27d712571eea7d3848e31b828478d
SpudManTwo <spudmantwo@gmail.com>
2020-08-25 23:26:42 -0400

    Adding UMD-replace library and redid EasyPatch for Nth time.

--- KPT/EasyPatch.cs
+++ KPT/EasyPatch.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -54,17 +55,17 @@ namespace KPT
 
         private void btnOutputPath_Click(object sender, EventArgs e)
         {
-            dlgOutputPath.SelectedPath = "";
-            dlgOutputPath.Description = "Please Select where you would like the patched iso files to go.";
-
+            dlgOutputPath.FileName = "";
+            dlgOutputPath.Filter = "ISO|*.iso";
+            dlgOutputPath.Title = "Please Select where you would like the patched iso file to go.";
             dlgOutputPath.ShowDialog();
 
-            if (dlgOutputPath.SelectedPath == "")
+            if (dlgOutputPath.FileName == "")
             {
                 return;
             }
 
-            txtOutputPath.Text = dlgOutputPath.SelectedPath;
+            txtOutputPath.Text = dlgOutputPath.FileName;
 
             ValidateForm(sender, e);
         }
@@ -83,7 +84,7 @@ namespace KPT
                 return;
             }
 
-            if (txtOutputPath.Text == "" || !Directory.Exists(txtOutputPath.Text))
+            if (txtOutputPath.Text == "")
             {
                 btnPatch.Enabled = false;
                 return;
@@ -114,6 +115,88 @@ namespace KPT
         {
             ZipArchive patchDirectory = ZipFile.OpenRead(txtPatchPath.Text);
 
+            if (File.Exists(txtOutputPath.Text))
+            {
+                File.Delete(txtOutputPath.Text);
+            }
+
+            File.Copy(txtIsoPath.Text, txtOutputPath.Text);
+
+            IEnumerable<ZipArchiveEntry> prebuiltFiles = patchDirectory.Entries.Where(patchEntry => patchEntry.Name.EndsWith(".cpk") || patchEntry.Name.EndsWith(".SFO"));
+
+            try
+            {
+                filesProcessed = 0;
+                totalFiles = (double)prebuiltFiles.Count();
+
+                foreach(ZipArchiveEntry patchFile in prebuiltFiles)
+                {
+                    //Generate a temp file path
+                    var tempFilePath = Path.GetTempFileName();
+                    //Delete the auto generated file from calling the GetTempFileName
+                    File.Delete(tempFilePath);
+                    //Extract the patched file to the temp file.
+                    patchFile.ExtractToFile(tempFilePath);
+
+                    //Run UMD-Replace to replace the file.
+                    ProcessStartInfo processStartInfo = new ProcessStartInfo();
+                    processStartInfo.UseShellExecute = false;
+                    processStartInfo.CreateNoWindow = true;
+                    processStartInfo.FileName = AppDomain.CurrentDomain.BaseDirectory + "Libraries\\UMD-replace\\UMD-replace.exe";
+                    processStartInfo.Arguments = "\""+txtOutputPath.Text +"\" \""+ patchFile.FullName.Substring(patchFile.FullName.IndexOf("PSP_GAME")).Replace("/", "\\") + "\" \""+ tempFilePath+"\"";
+                    var t = processStartInfo.ToString();
+                    var process = Process.Start(processStartInfo);
+                    process.WaitForExit();
+
+                    //If things broke, throw exception
+                    if(process.ExitCode != 0)
+                    {
+                        throw new Exception(process.StandardOutput.ReadToEnd());
+                    }
+
+                    //Delete the temp file now that we're done with it.
+                    File.Delete(tempFilePath);
+
+                    filesProcessed++;
+                    //Update Progress Bar
+                    PatchProgressBar.Value = (int)((filesProcessed / totalFiles) * 100);
+                }
+
+                MessageBox.Show("That was easy.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was an issue. Please send the log located at " + logFilePath + " to SpudManTwo and yell at him to fix things.");
+                using (System.IO.StreamWriter logFile = new System.IO.StreamWriter(logFilePath, true))
+                {
+                    logFile.WriteLine("Exception hit:\n" + ex.ToString());
+                }
+            }
+            finally
+            {
+                patchDirectory.Dispose();
+
+                //Re-Enable text boxes
+                txtIsoPath.Enabled = true;
+                btnIsoPath.Enabled = true;
+                txtPatchPath.Enabled = true;
+                btnPatchPath.Enabled = true;
+                txtOutputPath.Enabled = true;
+                btnOutputPath.Enabled = true;
+
+                System.Timers.Timer renablePatchTier = new System.Timers.Timer(2000);
+                renablePatchTier.Elapsed += ReenablePatchButton;
+                renablePatchTier.AutoReset = false;
+                renablePatchTier.Enabled = true;
+            }
+
+            Close();
+        }
+
+        private async Task PatchGameDeprecated()
+        {
+            ZipArchive patchDirectory = ZipFile.OpenRead(txtPatchPath.Text);
+
             var isoReader = new ISOReader();
 
             if (!isoReader.OpenISOStream(txtIsoPath.Text, txtOutputPath.Text))
@@ -134,7 +217,7 @@ namespace KPT
 
                 //First We Dump the Iso and make list of files in need of patching.
 
-                List<Task> tasks = isoFiles.Select(file => PatchFile(file, prebuiltFiles)).ToList();
+                List<Task> tasks = isoFiles.Select(file => PatchFileDeprecated(file, prebuiltFiles)).ToList();
 
                 await Task.WhenAll(tasks);
 
@@ -172,7 +255,7 @@ namespace KPT
             btnPatch.Enabled = true;
         }
 
-        private async Task PatchFile(EmbeddedFileAccessor file, IEnumerable<ZipArchiveEntry> prebuiltFiles)
+        private async Task PatchFileDeprecated(EmbeddedFileAccessor file, IEnumerable<ZipArchiveEntry> prebuiltFiles)
         {
             using (System.IO.StreamWriter logFile = new System.IO.StreamWriter(logFilePath, true))
             {
